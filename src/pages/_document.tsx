45356e01cdfc56a03c604f2d4a6a1a8613712c6e
fabiof2x <fabioar@gmail.com>
2022-05-13 14:51:26 -0300

    feat: colors variables and typography

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,900&family=Poppins:wght@400;600&family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
