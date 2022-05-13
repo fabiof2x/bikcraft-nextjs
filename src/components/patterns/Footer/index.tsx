7bc8652c3b65108acab6712dbeb870797bf15d6b
fabiof2x <fabioar@gmail.com>
2022-05-13 16:48:47 -0300

    feat: footer

---
+++ src/components/patterns/Footer/index.tsx
@@ -0,0 +1,44 @@
+import { Link } from "../../Link";
+import styles from "./Footer.module.scss";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footerBg}>
+      <div className={styles.footerContainer}>
+        <img src="./img/bikcraft.svg" width="136" height="32" alt="Bikcraft" />
+        <div className={styles.footerContato}>
+          <h3>Contato</h3>
+          <ul>
+            <li><a href="tel:+556199999999">+55 61 9999-9999</a></li>
+            <li><a href="mailto:contato@bikcraft.com">contato@bikcraft.com</a></li>
+            <li>Aquela Rua, 42 - Guará II</li>
+            <li>Brasília - DF</li>
+          </ul>
+          <div className={styles.footerRedes}>
+            <a href="https://www.instagram.com/fabiof2x/" target="_blank">
+              <img src="./img/redes/instagram.svg" width="32" height="32" alt="Instagram" />
+            </a>
+            <a href="https://facebook.com/fabiof2x/" target="_blank">
+              <img src="./img/redes/facebook.svg" width="32" height="32" alt="Facebook" />
+            </a>
+            <a href="#">
+              <img src="./img/redes/youtube.svg" width="32" height="32" alt="Youtube" />
+            </a>
+          </div>
+        </div>
+        <div className={styles.footerInformacoes}>
+          <h3>Informações</h3>
+          <nav>
+            <ul>
+              <li><Link href="/bicicletas">Bicicletas</Link></li>
+              <li><Link href="/seguros">Seguros</Link></li>
+              <li><Link href="/contato">Contato</Link></li>
+              <li><Link href="/termos">Termos e Condições</Link></li>
+            </ul>
+          </nav>
+        </div>
+        <p className={styles.footerCopy}>Bikcraft © Alguns direitos reservados.</p>
+      </div>
+    </footer>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import { PageTitle } from "../components/PageTitle"
+import Footer from "../components/patterns/Footer"
 import Header from "../components/patterns/Header"
 
 function HomePage() {
   return (
     <>
+      <PageTitle
+        title="Bikcraft - Bicicletas Elétricas"
+        description="Bicicletas elétricas de alta precisão e qualidade, feitas sob medida para você. Explore o mundo na sua velocidade com a Bikcraft."
+      />
       <Header />
+      <Footer />
     </>
   )
 }
