f9496bcee1987548138e222e6146f290dc57911e
fabiof2x <fabioar@gmail.com>
2022-05-13 15:42:50 -0300

    feat: link component

---
+++ src/components/Link/index.tsx
@@ -0,0 +1,21 @@
+import NextLink, { LinkProps } from 'next/link';
+import { ReactNode } from 'react';
+
+interface Props extends LinkProps {
+  children: ReactNode,
+  href: string,
+}
+
+export function Link({
+  children,
+  href,
+  ...props
+}: Props) {
+  return (
+    <NextLink href={href} passHref>
+      <a {...props}>
+        {children}
+      </a>
+    </NextLink>
+  );
+}
