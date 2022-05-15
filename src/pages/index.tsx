b86ae6c2560f7d94dfb45419d968be0399d0a4df
fabiof2x <fabioar@gmail.com>
2022-05-15 18:16:04 -0300

    feat(home): intro section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { PageTitle } from "../components/PageTitle"
+
+import styles from "../styles/Home.module.scss";
+
 import Footer from "../components/patterns/Footer"
 import Header from "../components/patterns/Header"
 
@@ -10,6 +13,21 @@ function HomePage() {
         description="Bicicletas elétricas de alta precisão e qualidade, feitas sob medida para você. Explore o mundo na sua velocidade com a Bikcraft."
       />
       <Header />
+
+      <main className={styles.introBg}>
+        <div className={styles.introContainer}>
+          <div className={styles.introContent}>
+            <h1>Bicicletas feitas sob medida<span>.</span></h1>
+            <p>Bicicletas elétricas de alta precisão e qualidade, feitas sob medida para você. Explore o mundo na sua velocidade com a Bikcraft.</p>
+            <a href="/bicicletas">Escolha a sua</a>
+          </div>
+          <picture>
+            <source media="(max-width: 800px)" srcSet="./img/bicicletas/nimbus.jpg" />
+            <img src="./img/fotos/introducao.jpg" width="1280" height="1600" alt="Bicicleta elétrica preta." />
+          </picture>
+        </div>
+      </main >
+
       <Footer />
     </>
   )
