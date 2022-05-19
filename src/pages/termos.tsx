36ddc9cdb11190322ef83d6728303dbe03c934c1
fabiof2x <fabioar@gmail.com>
2022-05-19 17:19:42 -0300

    feat: terms and conditions page

--- src/pages/termos.tsx
+++ src/pages/termos.tsx
@@ -20,6 +20,17 @@ export default function Terms() {
           subtitle="Termos de uso"
           title="termos e condições"
         />
+
+        <div className={styles.termsContainer}>
+          <h2>1. Política de Estorno</h2>
+          <p>1.1 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitamos PIX e Boleto.</p>
+          <p>1.2 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitamos PIX e Boleto.</p>
+          <p>1.3 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitamos PIX e Boleto.</p>
+          <h2>2. Conta do Usuário</h2>
+          <p>2.1 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitamos PIX e Boleto.</p>
+          <h2>3. Seguro Bikcraft</h2>
+          <p>3.1 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitamos PIX e Boleto.</p>
+        </div>
       </main>
 
       <Footer />
