3c9916bdd804942e635ab8c0d55e2fdafe99ebb4
fabiof2x <fabioar@gmail.com>
2022-05-22 13:28:47 -0300

    feat(insurance): faq section

---
+++ src/dtos/QuestionDTO.ts
@@ -0,0 +1,5 @@
+export interface QuestionDTO {
+  id: string,
+  question: string,
+  answer: string,
+}
--- src/pages/seguros.tsx
+++ src/pages/seguros.tsx
@@ -1,13 +1,48 @@
+import { Disclosure } from "@headlessui/react";
 import { DocumentTitle } from "../components/DocumentTitle"
 
 import Footer from "../components/patterns/Footer"
 import Header from "../components/patterns/Header"
 import HeaderTitle from "../components/patterns/HeaderTitle";
 import InsurancePlans from "../components/patterns/InsurancePlans";
+import { QuestionDTO } from "../dtos/QuestionDTO";
 
 import styles from "../styles/Insurance.module.scss";
 
 export default function Insurance() {
+  //Temp Bikes Data
+  const questions: QuestionDTO[] = [
+    {
+      id: "1",
+      question: "Qual forma de pagamento vocês aceitam?",
+      answer: "Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à vista também aceitarmos PIX e Boleto através do PagSeguro.",
+    },
+    {
+      id: "2",
+      question: "Como posso entrar em contato?",
+      answer: "Através de nosso formulário de contato ou dos telefones que informamos na nossa página de contato.",
+    },
+    {
+      id: "3",
+      question: "Vocês possuem algum desconto?",
+      answer: "Todos os nossos produtos possuem desconto de 10% em pagamentos via PIX ou à vista no Cartão de Crédito.",
+    },
+    {
+      id: "4",
+      question: "Qual a garantia que possuo?",
+      answer: "Garantia de 1 ano em qualquer componente elétrico e de 2 anos nos acessórios.",
+    },
+    {
+      id: "5",
+      question: "Posso parcelar no boleto?",
+      answer: "Você pode parcelar em até 12x sem juros.",
+    },
+    {
+      id: "6",
+      question: "Quantas trocas posso fazer ao ano?",
+      answer: "Você pode fazer até 2 (duas) trocas no plano PRATA e até 5 (cinco) trocas no plano OURO.",
+    },
+  ];
   return (
     <>
       <DocumentTitle
@@ -63,6 +98,29 @@ export default function Insurance() {
         </div>
       </article>
 
+      <article className={styles.faqContainer}>
+        <h2>perguntas frequentes<span>.</span></h2>
+
+        <dl>
+          {questions.map(question => (
+            <Disclosure key={question.id}>
+              {({ open }) => (
+                <div>
+                  <dt>
+                    <Disclosure.Button>
+                      <span>{question.question}</span>
+                    </Disclosure.Button>
+                  </dt>
+                  <Disclosure.Panel as="dd">
+                    {question.answer}
+                  </Disclosure.Panel>
+                </div>
+              )}
+            </Disclosure>
+          ))}
+        </dl>
+
+      </article>
 
       <Footer />
     </>
