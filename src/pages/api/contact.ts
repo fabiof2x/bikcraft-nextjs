54867b07e129e0a7691e16f1c85ef43d8ea32884
fabiof2x <fabioar@gmail.com>
2022-07-04 13:58:33 -0300

    feat: write contact form data on external db

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,3 +1,4 @@
+import { prisma } from "../../../lib/prisma";
 import { addDoc, collection } from "firebase/firestore";
 import { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../services/firebase";
@@ -14,15 +15,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     const contactData: Inputs = req.body;
 
-    const docRef = await addDoc(collection(db, "contato"), {
-      nome: contactData.name,
-      areaCode: contactData.areaCode,
-      phone: contactData.phone,
-      email: contactData.email,
-      message: contactData.message,
-    });
+    const contactMessage: Inputs = await prisma.contact.create({
+      data: {
+        name: contactData.name,
+        areaCode: contactData.areaCode,
+        phone: contactData.phone,
+        email: contactData.email,
+        message: contactData.message,
+      },
+    })
 
-    return res.status(200).json({ data: docRef })
+    return res.status(200).json({ data: contactMessage })
   } else {
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method not allowed')
